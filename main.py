7efb15bc273297b88bd84be950f2fc03a57e0bb6
Over-haven <tanushdas26@gmail.com>
2024-10-27 20:47:07 +0530

    Htc

---
+++ main.py
@@ -0,0 +1,25 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(a != c)
+
+a = "python"
+b = "codingal"
+
+if a != b:
+    print(a,'and',b,'are different')
+  
+  a=4
+  b=5
+
+if (a=1) != (b=5):
+  print('Hellow')
+
+
+
+a = int(input("enter a number"))
+
+if a%2 != 0:
+  print(a,"is not a even number")
